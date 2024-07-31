b54480d7391a916ea59ea3a4bf23181e8668322c
duynguyenbui <buiduy.ng@gmail.com>
2024-07-31 09:54:03 +0700

    final

--- async-reader/main.go
+++ async-reader/main.go
@@ -37,7 +37,7 @@ func main() {
 }
 
 func insertsHotels(conn *pgx.Conn) error {
-	f, err := os.Open("partner_feed_en_v3_minimal.jsonl.zst")
+	f, err := os.Open("partner_feed_en_v3.jsonl.zst")
 	if err != nil {
 		return fmt.Errorf("os.Open %w", err)
 	}
